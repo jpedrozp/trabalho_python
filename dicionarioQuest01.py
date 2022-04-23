092b423397bacbca5ff4a09fdc924c242de378d6
Catarina Lima <catarinalimalss@gmail.com>
2022-04-23 20:50:19 -0300

    Update dicionarioQuest01.py

--- dicionarioQuest01.py
+++ dicionarioQuest01.py
@@ -1,1 +1,14 @@
-#n
+# Questão 01 - Escreva um programa que permita gerenciar o banco de dados de clientes de uma empresa. 
+# Os clientes serão salvos em um dicionário no qual a chave de cada cliente será seu CPF, 
+# e o valor será outro dicionário com os dados do cliente (nome, endereço, telefone, email, preferencial), 
+# onde preferencial terá o valor True se for de um cliente especial. 
+# O programa deve solicitar ao usuário uma opção no seguinte menu: 
+# (1) Adicionar cliente, (2) Remover cliente, (3) Mostrar cliente, (4) Listar todos os clientes, (5) Listar clientes preferenciais, (6) Terminar. 
+# Dependendo da opção escolhida, o programa deverá fazer o seguinte:
+# Peça dados do cliente, crie um dicionário com os dados e adicione-o ao banco de dados.
+# Peça o CPF do cliente e exclua seus dados do banco de dados.
+# Peça o CPF do cliente e mostre seus dados.
+# Mostrar lista de todos os clientes no banco de dados com seu CPF e nome.
+# Mostre a lista de clientes preferenciais no banco de dados com seu CPF e nome.
+# Termine o programa.
+
